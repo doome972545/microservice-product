f3b071d66e23be65287571a5b4aca2d98e827a62
Doome <domedoome9725451@gmail.com>
2025-08-17 01:34:05 +0700

    fix database

--- product-service/src/test/java/com/Doome/microservice/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/Doome/microservice/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package com.Doome.microservice.product_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost"; // ✅ ใช้ localhost แทน IP
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name":"iPhone 15",
+                    "description":"iPhone 15 is A smartphone from Apple",
+                    "price":1100
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("iPhone 15"))
+                .body("description", Matchers.equalTo("iPhone 15 is A smartphone from Apple"))
+                .body("price", Matchers.equalTo(1100));
+    }
 }
